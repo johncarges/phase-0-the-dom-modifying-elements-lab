7cc4241f2d7b6605ef4c8e6c4af14483e2006fa4
John Carges <johncarges@gmail.com>
2023-06-18 11:45:13 -0400

    John Carges is the champion

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "John Carges is the champion";
+
+const body = document.getElementsByTagName("body")[0];
+body.append(newHeader);
